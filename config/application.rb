fb01e7dc2bd9f09b7687fbb118562d8858fce794
George Garcia <jorge.90gap@gmail.com>
2022-01-23 18:14:44 +0900

    internationalization and time zone settings

--- config/application.rb
+++ config/application.rb
@@ -10,15 +10,25 @@ module Mayo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :en
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.jbuilder false
+    end
+
     config.generators do |g|
       g.test_framework :rspec,
-                        model_specs: true,
-                        view_specs: false,
-                        helper_specs: false,
-                        routing_specs: false,
-                        controller_specs: false,
-                        request_specs: false
+        model_specs: true,
+        view_specs: false,
+　　　　　helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
     end
   end
 end
