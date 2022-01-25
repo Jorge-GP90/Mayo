707acadc206d75455f45b84aa259b107100a04fb
George Garcia <jorge.90gap@gmail.com>
2022-01-25 14:40:10 +0900

    Status Enum setted up

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,10 @@
 class Task < ApplicationRecord
-  validates :task_name, :description, presence: true
+  before_validation :set_default_expired, on: %i[ create update ]
+  validates :task_name, :description, :status, presence: true
+  
+  require './app/orderclass/status'
+  enum status: Status.options_for_enum
+  
   private
   def set_default_expired
     self.expired_at = Time.now if expired_at.blank?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_134544) do
+ActiveRecord::Schema.define(version: 2022_01_25_051932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,10 +19,10 @@ ActiveRecord::Schema.define(version: 2022_01_24_134544) do
     t.string "task_name", null: false
     t.text "description", null: false
     t.string "priority"
-    t.string "status"
     t.datetime "expired_at", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0, null: false
   end
 
 end
