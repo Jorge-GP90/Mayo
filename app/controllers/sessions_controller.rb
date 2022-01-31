c7587ae6391c2e1770c9c3bdbb10db273022275e
George Garcia <jorge.90gap@gmail.com>
2022-01-31 20:35:26 +0900

    fix seed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    user = User.find_by(email: session_params[:email].downcase)
-    if user && user.authenticate(session_params[:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
